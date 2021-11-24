7513ee63f34ce7f142c8a4bde0d779ccebac3fbd
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-11-24 02:46:48 -0600

    Agregar carpeta variables que contiene un archivo donde damos introduccion a las variables en ruby

---
+++ variables/variables.rb
@@ -0,0 +1,17 @@
+# El tipo de una variable se define por el valor de esta, ya que ruby es un lenguaje interpretado
+planeta = "Tierra" # Variable tipo -> string
+años = 128391 # Variable tipo -> integer
+pi = 3.1416 # variable tipo -> float
+
+
+# snake_case -> es el formato para declarar las variables en ruby
+nombre_del_programador = "Jorge"
+
+# maneras no validas de iniciar una variable
+# - No se deben de inicializar con numeros
+# - No dedebn incluir palabras reservadas
+# - Tomar en cuenta que si se inicializa una variable con una letra mayuscula esta se tomara como una constante
+edad = 29
+
+
+puts nombre_del_programador, edad
